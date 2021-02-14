1e4f3e9ee57ac8c99499b85cfba95cc5d180555f
Brian Gallops <gallob75@gmail.com>
2021-02-13 23:43:08 -0600

    Set up basic framework for story telling, add a partial sample story, pick an appropriate font, organize folders a bit

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,30)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory() {
+        return storyText;
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+}
