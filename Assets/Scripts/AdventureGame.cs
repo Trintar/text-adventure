60b662a735068d618a9a4cf91ba4e5f82777d980
Brian Gallops <gallob75@gmail.com>
2021-02-14 22:10:44 -0600

    Clean up a couple states and refactor choice input handling

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -33,12 +33,10 @@ public class AdventureGame : MonoBehaviour
     private void ManageState() {
         var nextStates = state.GetNextStates();
 
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            state = nextStates[0];
-        } else if (Input.GetKeyDown(KeyCode.Alpha2) && nextStates.Length >= 2) {
-            state = nextStates[1];
-        } else if (Input.GetKeyDown(KeyCode.Alpha3) && nextStates.Length >= 3) {
-            state = nextStates[2];
+        for (int i = 0; i < nextStates.Length; i++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i)) {
+                state = nextStates[i];
+            }
         }
 
         textComponent.text = state.GetStateStory();
